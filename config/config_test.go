8e9bba6605d9ba3786d3a5f9d947ce793a61f395
Eleanor Deal <eleanor.deal@gmail.com>
2018-08-28 12:30:36 +0100

    Update README to reflect config

--- config/config_test.go
+++ config/config_test.go
@@ -27,7 +27,6 @@ func TestSpec(t *testing.T) {
 				So(cfg.DownloadServiceSecretKey, ShouldEqual, "QB0108EZ-825D-412C-9B1D-41EF7747F462")
 				So(cfg.WebsiteURL, ShouldEqual, "http://localhost:20000")
 				So(cfg.ZebedeeURL, ShouldEqual, "http://localhost:8082")
-				So(cfg.AuditEventsTopic, ShouldEqual, "audit-events")
 				So(cfg.ServiceAuthToken, ShouldEqual, "FD0108EA-825D-411C-9B1D-41EF7727F465")
 				So(cfg.GracefulShutdownTimeout, ShouldEqual, 5*time.Second)
 				So(cfg.Neo4jBindAddress, ShouldEqual, "bolt://localhost:7687")
